6da7f16d16c06f0a869f902f714383da1023c72b
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-01-06 14:46:52 -0700

    typo

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -4,4 +4,4 @@ const knex = require("knex");
 const config = require("../knexfile.js");
 const environment = process.env.DB_CONNECT || "development";
 
-module.exports = knex(config[environment]);
+module.exports = knex(config[environment])
