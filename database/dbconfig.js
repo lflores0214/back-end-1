ae09916694177dbc666a500e5257adf81fc5f188
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-01-06 14:26:29 -0700

    updated dbconfig file

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,5 +1,7 @@
-const knex = require('knex');
+require('dotenv').config();
 
-const knexConfig = require('../knexfile.js');
+const knex = require("knex");
+const config = require("../knexfile.js");
+const environment = process.env.DB_CONNECT || "development";
 
-module.exports = knex(knexConfig.development)
+module.exports = knex(config[environment]);
