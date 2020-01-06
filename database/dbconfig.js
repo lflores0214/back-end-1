c45f091a9224f3abc64780b95b35bb3108384cd6
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-01-06 14:48:16 -0700

    removed file

--- database/dbconfig.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const knex = require("knex");
-const config = require("../knexfile.js");
-const environment = process.env.DB_CONNECT || "development";
-
-module.exports = knex(config[environment])
