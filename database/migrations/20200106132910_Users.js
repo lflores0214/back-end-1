03706c8deb87c382ae5fdb93d20fd6dd1525031e
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-01-06 13:32:02 -0700

    added migrations

---
+++ database/migrations/20200106132910_Users.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+
+        users.string("username", 20)
+        .notNullable()
+        .unique()
+
+        users.string("email", 128)
+        .notNullable
+
+        users.string("password", 24)
+        .notNullable()
+
+    })
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("users");
+};
